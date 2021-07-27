0f0a6cebbf83f8005a350a0cd9f4609a5b9f1535
Julianne Agresta <jragresta@gmail.com>
2021-07-27 17:15:26 -0400

    Todo App created with VueJS

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde cum aut natus a dolores, consequuntur alias possimus saepe aperiam nulla accusamus rem quisquam fuga rerum pariatur ex fugiat culpa asperiores?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde cum aut natus a dolores, consequuntur alias possimus saepe aperiam nulla accusamus rem quisquam fuga rerum pariatur ex fugiat culpa asperiores?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde cum aut natus a dolores, consequuntur alias possimus saepe aperiam nulla accusamus rem quisquam fuga rerum pariatur ex fugiat culpa asperiores?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde cum aut natus a dolores, consequuntur alias possimus saepe aperiam nulla accusamus rem quisquam fuga rerum pariatur ex fugiat culpa asperiores?</p>
   </div>
 </template>
